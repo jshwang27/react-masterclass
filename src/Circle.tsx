615c15b5a1476ecef4a601ea44fad0c7d5ef6bfe
jshwang27 <jshwang27@gmail.com>
2022-02-13 02:31:04 +0900

    #0.8 react module install

---
+++ src/Circle.tsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+interface ContainerProps {
+  bgColor: string;
+  borderColor: string; //requierd
+}
+
+const Container = styled.div<ContainerProps>`
+  width: 200px;
+  height: 200px;
+  background-color: ${(props) => props.bgColor};
+  border-radius: 100px;
+  border: 1px solid ${(props) => props.borderColor}; // interface 에 있기 때문에 넣어 주어야 함
+`;
+
+interface CircleProps {
+  bgColor: string;
+  borderColor?: string; // 없어도 됨.
+  text?: string; // 기본값 설정 할 때
+}
+
+function Circle({ bgColor, borderColor, text = "default text" }: CircleProps) {
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {text}
+    </Container>
+  ); // borderColor에 기본값 설정
+}
+
+interface PlayerShape {
+  name: string;
+  age: number;
+}
+
+const sayHello = (playerObj: PlayerShape) =>
+  `Hello ${playerObj.name} you are ${playerObj.age} years old. `;
+
+sayHello({ name: "name", age: 12 });
+// sayHello({ name: "name2", age: 22, hello: "hello" });
+
+export default Circle;
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ThemeProvider } from "styled-components";
 import App from "./App";
-
-// 테마(다크모드) 설정. 속성이 같아야 한다.
-const darkTheme = { textColor: "whiteSmoke", backgroundColor: "#111" };
-const lightTheme = { textColor: "#111", backgroundColor: "#whitesmoke" };
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      {/* 테마 속성을 넘겨주는 부분 */}
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
